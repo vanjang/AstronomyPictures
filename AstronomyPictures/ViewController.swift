e23c63368e95ac609e79bca4abcf0843913e4f52
Myung Hoon So <79270874+myunghoonso@users.noreply.github.com>
2024-01-21 07:49:22 +0000

    project set up

---
+++ AstronomyPictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstronomyPictures
+//
+//  Created by myung hoon on 21/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
